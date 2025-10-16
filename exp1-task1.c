0d28fb5ec6656737106ce558bb86593fa2304cd6
skayaan27 <skayaan@eng.rizvi.edu.in>
2025-10-17 00:08:54 +0530

    Create exp1-task1.c

---
+++ exp1-task1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+   printf("NAME-SHAIKH AYAAN NOOR MOHAMMAD\n");
+   printf("AGE-18\n");
+   printf("CLASS-COMPUTER ENGG\n");
+   printf("DIV-C\n");
+   printf("UIN-251P001\n");
+    return 0;
+}
